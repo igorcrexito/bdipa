ddb7aafef3519bafcc78376527486bfbde9a5808
bernardo.uefs@gmail.com <bernardo.uefs@gmail.com@b41930ff-aed5-1685-a4d5-ade27e0bb7af>
2011-07-18 02:31:01 +0000

    1ª versão

---
+++ trunk/BancoImagens/src/java/Banco/Conexao/PacienteDAO.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Banco.Conexao;
+
+import DAO.Paciente;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author odontoradiosis
+ */
+public class PacienteDAO {
+    
+   private Paciente paciente;
+   private ConexaoBD conexao;
+
+    public PacienteDAO(ConexaoBD conexao) {
+        this.conexao = conexao;
+    }
+
+    public Paciente getPacienteFromId(int id) throws SQLException {
+
+        String SQL_string = "SELECT * FROM pacientes WHERE " +
+                "Id LIKE '%" + id + "%'";
+
+        ResultSet rs= conexao.executeSql(SQL_string);
+        rs.first();
+
+        int sexo = rs.getInt("sexo");
+        int idade = rs.getInt("idade");
+        String urlImagem = rs.getString("urlImagem");
+
+        paciente = new Paciente(id,sexo,idade,urlImagem);
+
+        return paciente;
+    }
+
+
+    public void inserirPaciente(int id, int sexo, int idade, String urlImagem) {
+
+        String SQL_String = "INSERT INTO pacientes (id, sexo, idade, urlImagem)" +
+                " VALUES ('" + id + "', '" + sexo + "', '" + idade + "', '" + urlImagem+"')";
+
+         conexao.execute(SQL_String);
+    }
+
+
+    public void excluirPaciente(int id) throws SQLException{
+
+        String SQL_String = "delete from pacientes where id='" + id + "'";
+        conexao.executeSql(SQL_String);
+    }
+
+}
