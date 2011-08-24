f0f1f70bfd192ff0da275df75febb29c595dbba6
igorcrexito@gmail.com <igorcrexito@gmail.com@b41930ff-aed5-1685-a4d5-ade27e0bb7af>
2011-08-24 16:42:09 +0000

    Parte de edição de páginas ajustada

--- BancoImagens/src/java/beans/pagesBean.java
+++ BancoImagens/src/java/beans/pagesBean.java
@@ -6,7 +6,12 @@ package beans;
  */
 
 import Banco.Conexao.ConexaoBD;
-import Banco.Conexao.ConteudoDAO;
+import Banco.Conexao.PaginaDAO;
+import classes.Pagina;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 
@@ -14,66 +19,108 @@ import javax.faces.bean.ManagedBean;
  *
  * @author Rafael
  */
-@ManagedBean
+@ManagedBean(name="pagBean")
 @ApplicationScoped
+
 public class pagesBean {
 
-    private String campo1,campo2,campo3,TituloTextoCampo1;
-    ConteudoDAO conteudoDAO = new ConteudoDAO(ConexaoBD.getConexaoBD());
+    private ArrayList<String> conteudos = new ArrayList<String>();
+    private int referencia;
+    private String campoAtual;
+    PaginaDAO pagDAO = new PaginaDAO();
+    private String nome;
+    public String campo1,campo2,campo3,campo4;
+
+    public pagesBean () {
+       conteudos.add(campo1);conteudos.add(campo2);conteudos.add(campo3);conteudos.add(campo4);
+    }
+
+    public void carregaTexto() {
+        Pagina pag = null;
+        try {
+            pag = pagDAO.getPaginaFromNome(nome);
+        } catch (SQLException ex) {
+            Logger.getLogger(pagesBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        if (pag!=null) {
+            this.conteudos = pag.getConteudo();
+        }
+        campoAtual = conteudos.get(referencia);
+    }
+
+    public void salvaTexto() {
+        this.conteudos.set(referencia, campoAtual);
+        pagDAO.updateConteudo(nome, conteudos);
+    }
+   
+    public void acionaHome() {
+        nome = "home";
+        Pagina pag=null;
+        try {
+            pag = pagDAO.getPaginaFromNome(nome);
+        } catch (SQLException ex) {
+            Logger.getLogger(pagesBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+         nome = pag.getNome();
+         this.conteudos = pag.getConteudo();
+    }
+
+     public void acionaPrincipaisTeoricos() {
+        nome = "principaisteoricos";
+        Pagina pag=null;
+        try {
+            pag = pagDAO.getPaginaFromNome(nome);
+        } catch (SQLException ex) {
+            Logger.getLogger(pagesBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+         nome = pag.getNome();
+         this.conteudos = pag.getConteudo();
+    }
+
+    public int getReferencia() {
+        return referencia;
+    }
+
+    public void setReferencia(int referencia) {
+        this.referencia = referencia;
+    }
+
+    public String getCampoAtual() {
+        return campoAtual;
+    }
+
+    public void setCampoAtual(String campoAtual) {
+        this.campoAtual = campoAtual;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
     public String getCampo1() {
-        try {
-         
-          campo1= conteudoDAO.getConteudoRefencia("1").getTexto();
-
-        } catch (Exception ex) {
-           
-            //Logger.getLogger(UserBean.class.getName()).log(Level.SEVERE, null, ex);
-        }
-       
+        campo1 = this.conteudos.get(0);
         return campo1;
-    }
-    public String getTituloTextoCampo1(){
-     try {
-
-          TituloTextoCampo1= conteudoDAO.getConteudoRefencia("1").getTituloTexto();
-
-        } catch (Exception ex) {
-
-            //Logger.getLogger(UserBean.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        return TituloTextoCampo1;
-
-    }
-
-    public void setCampo1(String campo1) {
-        this.campo1 = campo1;
     }
 
     public String getCampo2() {
-
-
+        campo2 = this.conteudos.get(1);
         return campo2;
     }
 
-    public void setCampo2(String campo2) {
-        this.campo2 = campo2;
-    }
-
     public String getCampo3() {
+        campo3 = this.conteudos.get(2);
         return campo3;
     }
 
-    public void setCampo3(String campo3) {
-        this.campo3 = campo3;
+    public String getCampo4() {
+        campo4 = this.conteudos.get(3);
+        return campo4;
     }
-
-    /** Creates a new instance of pagesBean */
-
-    public pagesBean() {
-
-    }
-
 
 }
