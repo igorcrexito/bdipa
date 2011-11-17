82aca30b6fded30eabbecd1415d6e3c4989f5bed
igorcrexito@gmail.com <igorcrexito@gmail.com@b41930ff-aed5-1685-a4d5-ade27e0bb7af>
2011-11-17 17:40:44 +0000

    Página de Admin pronta

--- trunk/BancoImagens/src/java/beans/UserBean.java
+++ trunk/BancoImagens/src/java/beans/UserBean.java
@@ -18,7 +18,6 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-import javax.faces.event.ComponentSystemEvent;
 import org.apache.commons.mail.EmailException;
 
 /**
